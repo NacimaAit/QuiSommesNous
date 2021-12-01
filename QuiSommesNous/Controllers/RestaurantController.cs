34e73c4b4f3cc87391f79533be23512b5c8edbcc
aitbe <aitbe@LAPTOP-6HHP65VJ>
2021-12-01 11:51:42 -0500

    Ajoutez des fichiers projet.

---
+++ QuiSommesNous/Controllers/RestaurantController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuiSommesNous.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuiSommesNous.Controllers
+{
+    
+    public class RestaurantController : Controller
+    {
+        static private List<Plat> plats = new List<Plat>()
+    {
+        new Plat (1,"Couscous", 50, "Oriental", "Fortement"),
+        new Plat (2,"Suchi", 40, "Asuatique", "Moyennement"),
+        new Plat (3,"Poulet Grec", 30, "Greque", "Faible"),
+        new Plat (4,"Soupe", 50, "asiatique", "Fortement"),
+    };
+        // GET: RestaurantController
+        public ActionResult Index()
+        {
+            return View(plats);
+        }
+
+        // GET: RestaurantController/Details/5
+        public ActionResult Details(int Id)
+        {
+            Plat detail = plats.Find(p => p.Id == Id);
+
+            return View(detail);
+        }
+
+        // GET: RestaurantController/Create
+        public ActionResult Create()
+        {
+            Plat  creer = new Plat();
+
+            return View(creer);
+        }
+
+        // POST: RestaurantController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Plat creer)
+        {
+            try
+            {
+                //ajouter un nouvelle enregistrement a la liste creer (plats)
+                plats.Add(creer);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RestaurantController/Edit/5
+        public ActionResult Edit(int Id)
+        {
+            Plat Edition = plats.Find(p => p.Id == Id);//chercher l identifiant
+
+            return View(Edition);
+        }
+
+        // POST: RestaurantController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int Id, Plat Edition)
+        {
+            try
+
+            {
+                var old = plats.Find(p => p.Id == Edition.Id);
+                plats.Remove(old);
+                plats.Add(Edition);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RestaurantController/Delete/5
+        public ActionResult Delete(int Id)
+        {
+            Plat supp = plats.Find(p => p.Id == Id);
+
+            return View(supp);
+        }
+
+        // POST: RestaurantController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int Id, Plat supp)
+        {
+            try
+            {
+                var old = plats.Find(p => p.Id == supp.Id);
+                plats.Remove(old);
+                
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ QuiSommesNous/Models/Plat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuiSommesNous.Models
+{
+    public class Plat
+    {
+      
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public int Prix { get; set; }
+        public string  Type { get; set; }
+        public string Recommandation { get; set; }
+
+        public Plat()
+        {
+
+        }
+
+        public Plat(int id, string nom, int prix, string type, string recommandation)
+        {
+            Id = id;
+            Nom = nom;
+            Prix = prix;
+            Type = type;
+            Recommandation = recommandation;
+        }
+    }
+
+}
